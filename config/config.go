85c81bd43e1c4690fc25f381e50315e74a00b477
jeanfo <719296013@qq.com>
2024-11-17 13:49:23 +0800

    add config parser

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type DatabaseConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	DBName   string `mapstructure:"dbname"`
+}
+
+type Config struct {
+	Database DatabaseConfig `mapstructure:"database"`
+}
+
+var AppConfig *Config
+
+func LoadConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	if err := viper.Unmarshal(&AppConfig); err != nil {
+		log.Fatalf("Effor unmarshaling config: %v", err)
+	}
+}
