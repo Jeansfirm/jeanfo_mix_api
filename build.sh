c08501fb6afaed28b28cf366378af2a7f86ca212
jeanfo <jeanfo@mac-air>
2025-03-29 10:34:19 +0800

    update build sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PROJECT_NAME=jeanfo_mix
+PROJECT_NAME_BASE=jeanfo_mix
+PROJECT_NAME=$PROJECT_NAME_BASE-`date "+%Y%m%d%H%M%S"`
 
 BUILD_DIR="build"
 OUTPUT_DIR="$BUILD_DIR/output"
@@ -16,7 +17,15 @@ echo ">> building the project..."
 # 动态链接库方式编译
 GOOS=linux GOARCH=amd64 go build -o "$OUTPUT_DIR/$PROJECT_NAME" cmd/main.go
 
-echo ">> copying cofig files..."
+
+# if real executable file does not exists, make soft_link to newest output
+if [ ! -L "$BUILD_DIR/$PROJECT_NAME_BASE" ];then
+    ln -sF $OUTPUT_DIR/$PROJECT_NAME $BUILD_DIR/$PROJECT_NAME_BASE
+fi
+
+
+# copy newest config file sample
+echo ">> copying config files..."
 mkdir -p $OUTPUT_DIR/config
 # cp -r config/*.yaml $OUTPUT_DIR/config/
 for file in config/*.yaml; do
@@ -25,6 +34,17 @@ for file in config/*.yaml; do
         cp $file $OUTPUT_DIR/config/${filename}.samle
     fi
 done
+
+
+# rm old and unlinked output files
+LINKED_FILE_PATH=`readlink $BUILD_DIR/$PROJECT_NAME_BASE`
+LINKED_FILE=`basename $LINKED_FILE_PATH`
+for file in $OUTPUT_DIR/$PROJECT_NAME_BASE*;do
+    if [[ "$file" != *$LINKED_FILE* && "$file" != *$PROJECT_NAME* ]];then
+        rm $file
+    fi
+done
+
 
 echo
 echo ">> build complete. output directory structure:"
